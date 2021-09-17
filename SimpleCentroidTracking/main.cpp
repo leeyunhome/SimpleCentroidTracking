43fb6e177599d18b79f86bf7a0123e87259ab78a
leeyunhome <leeyunhome@daum.net>
2021-09-17 10:09:05 +0900

    make a calcCentroid function

--- SimpleCentroidTracking/main.cpp
+++ SimpleCentroidTracking/main.cpp
@@ -6,6 +6,8 @@
 using namespace cv;
 using namespace std;
 
+void calcCentroid(int, int, int, int, int* a, int* b);
+
 int main()
 {
 	//printf("SimpleCentroidTracking");
@@ -31,8 +33,42 @@ int main()
 		cap.read(cameraFrame);
 
 		resize(cameraFrame, cameraFrame, Size(400, 300));
+		auto inputBlob = dnn::blobFromImage(cameraFrame, 1.0, Size(400, 300), Scalar(104.0, 177.0, 123.0));
+		
+		net.setInput(inputBlob);
+		auto detection = net.forward("detection_out");
+		Mat detectionMat(detection.size[2], detection.size[3], CV_32F, detection.ptr<float>());
+		
+		vector<vector<int>> boxes;
 
+		float confidenceThreshold = 0.2;
+		for (int i = 0; i < detectionMat.rows; i++)
+		{
+			float confidence = detectionMat.at<float>(i, 2);
+		
+			if (confidence > confidenceThreshold)
+			{
+				int xLeftTop = static_cast<int>(detectionMat.at<float>(i, 3) * cameraFrame.cols);
+				int yLeftTop = static_cast<int>(detectionMat.at<float>(i, 4) * cameraFrame.rows);
+				int xRightBottom = static_cast<int>(detectionMat.at<float>(i, 5) * cameraFrame.cols);
+				int yRightBottom = static_cast<int>(detectionMat.at<float>(i, 6) * cameraFrame.rows);
+
+				int centroid_x = 0;
+				int centroid_y = 0;
+
+				calcCentroid(xLeftTop, yLeftTop, xRightBottom, yRightBottom, &centroid_x, &centroid_y);
+
+				Rect object((int)xLeftTop, (int)yLeftTop, (int)(xRightBottom - yRightBottom),
+					(int)(yRightBottom - yLeftTop));
+
+				drawMarker(cameraFrame, Point(centroid_x, centroid_y), Scalar(255, 0, 0), MARKER_SQUARE);
+				rectangle(cameraFrame, object, Scalar(0, 255, 0), 2);
+
+				boxes.insert(boxes.end(), { xLeftTop, yLeftTop, xRightBottom, yRightBottom });
+			}
+		}
 		imshow("Detection", cameraFrame);
+
 		char c = (char)waitKey(15);
 		if (c == 27)
 			break;
@@ -41,4 +77,11 @@ int main()
 	return 0;
 }
 
+void calcCentroid(int xLeftTop, int yLeftTop, int xRightBottom, int yRightBottom, int* a, int* b)
+{
+	*a = xLeftTop + (xRightBottom - xLeftTop) * 0.5;
+	*b = yLeftTop + (yRightBottom - yLeftTop) * 0.5;
+
+	return;
+}
 
